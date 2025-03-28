eef55f3a88692ef33fa8521413941ceea0f03173
Oliver Lockwood <olockwood@dalet.com>
2025-03-28 09:53:40 +0000

    Typing tweaks to aid consumption in Typescript code; return value from set() method; add semicolons everywhere

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { AsyncLocalStorage } from 'async_hooks'
-import type { NextFunction, RequestHandler, Request, Response } from 'express'
+import { AsyncLocalStorage } from 'async_hooks';
+import type { NextFunction, Request, Response } from 'express';
+
 
 declare global {
 	// eslint-disable-next-line no-var
@@ -8,44 +9,60 @@ declare global {
 
 
 if (!global.expressHttpContextStorage) {
-	global.expressHttpContextStorage = new AsyncLocalStorage<Map<string, any>>()
+	global.expressHttpContextStorage = new AsyncLocalStorage<Map<string, any>>();
 }
 
 
-
 /**
- * Express.js middleware that is responsible for initializing the context for each request.
+ * Express.js middleware that is responsible for initializing the context for
+ * each request.
+ *
+ * Declared as type 'any' because otherwise Typescript consumers can get tie
+ * themselves in knots.
  */
-export const middleware: RequestHandler = (
+export const middleware: any = (
 	_req: Request,
 	_res: Response,
 	next: NextFunction
 ): void => {
-	global.expressHttpContextStorage.run(new Map(), () => next())
+	global.expressHttpContextStorage.run(new Map(), () => next());
 }
+
 
 /**
  * Gets a value from the context by key.
- * Will return undefined if the context has not yet been initialized for this request
- * or if a value is not found for the specified key.
+ *
+ * Will return undefined if the context has not yet been initialized for this
+ * request, or if a value is not found for the specified key.
  */
 export function get<T = any>(key: string): T | undefined {
 
 	const store = global.expressHttpContextStorage.getStore();
 
-	return store?.get(key)
+	return store?.get(key);
 }
+
 
 /**
  * Adds a value to the context by key.
+ *
  * If the key already exists, its value will be overwritten.
- * No value will persist if the context has not yet been initialized.
+ * No value will be persisted if the context has not yet been initialized.
+ *
+ * Returns the value that was set, or undefined if the context has not yet been
+ * initialized for this request.
  */
-export function set<T = any>(key: string, value: T): void {
+export function set<T = any>(key: string, value: T): T | undefined {
 
 	const store = global.expressHttpContextStorage.getStore();
 
-	store?.set(key, value)
+    if (store) {
+        store.set(key, value);
+        return value;
+    }
+
+    return undefined;
 }
+
 
 export default { get, middleware, set }
