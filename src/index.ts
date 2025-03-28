98fe5fea89cb6babc5629ae8666e63fa69c8a504
Oliver Lockwood <olockwood@dalet.com>
2025-03-28 08:42:09 +0000

    Try to only ever create one AsyncLocalStorage instance

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import { AsyncLocalStorage } from 'async_hooks'
-import type { NextFunction, Request, Response } from 'express'
+import type { NextFunction, RequestHandler, Request, Response } from 'express'
 
-const asyncLocalStorage = new AsyncLocalStorage<Map<string, any>>()
+declare global {
+	// eslint-disable-next-line no-var
+	var expressHttpContextStorage: AsyncLocalStorage<Map<string, any>>;
+}
+
+
+if (!global.expressHttpContextStorage) {
+	global.expressHttpContextStorage = new AsyncLocalStorage<Map<string, any>>()
+}
+
+
 
 /**
  * Express.js middleware that is responsible for initializing the context for each request.
  */
-export function middleware(
-	req: Request,
-	res: Response,
+export const middleware: RequestHandler = (
+	_req: Request,
+	_res: Response,
 	next: NextFunction
-): void {
-	asyncLocalStorage.run(new Map(), () => next())
+): void => {
+	global.expressHttpContextStorage.run(new Map(), () => next())
 }
 
 /**
@@ -20,7 +30,10 @@ export function middleware(
  * or if a value is not found for the specified key.
  */
 export function get<T = any>(key: string): T | undefined {
-	return asyncLocalStorage.getStore()?.get(key)
+
+	const store = global.expressHttpContextStorage.getStore();
+
+	return store?.get(key)
 }
 
 /**
@@ -29,7 +42,10 @@ export function get<T = any>(key: string): T | undefined {
  * No value will persist if the context has not yet been initialized.
  */
 export function set<T = any>(key: string, value: T): void {
-	asyncLocalStorage.getStore()?.set(key, value)
+
+	const store = global.expressHttpContextStorage.getStore();
+
+	store?.set(key, value)
 }
 
 export default { get, middleware, set }
